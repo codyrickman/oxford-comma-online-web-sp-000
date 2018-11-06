bd26669d810744ed56baf4259c4010f7a236ec57
Cody Rickman <cody@creativemindstudios.com>
2018-11-06 12:26:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
 def oxford_comma(array)
-  new_array = array.join(", ");
-  return new_array;
+  length = array.length - 1;
+  index =  (1..length).to_a;
+  new_array = [];
+  index.each do |index|
+    new_array << "#{array[index]}, ";
+  end
+  joined = new_array.join;
+  return "#{joined} and #{array[-1]}";
 end
