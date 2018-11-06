256d4f4dc61a5e576383b77ddcc1ad24c6023471
Cody Rickman <cody@creativemindstudios.com>
2018-11-06 12:28:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,16 @@
 def oxford_comma(array)
-  length = array.length - 1;
-  index =  (1..length).to_a;
-  new_array = [];
-  index.each do |index|
-    new_array << "#{array[index]}, ";
+  if array.length == 1
+    return array;
+  elsif array.length == 2
+    retrun array.join("and");
+  else
+    length = array.length - 1;
+    index =  (1..length).to_a;
+    new_array = [];
+    index.each do |index|
+      new_array << "#{array[index]}, ";
+    end
+    joined = new_array.join;
+    return "#{joined} and #{array[-1]}";
   end
-  joined = new_array.join;
-  return "#{joined} and #{array[-1]}";
 end
